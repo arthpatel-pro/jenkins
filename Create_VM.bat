7a99aa2c70f67bbf7961097ca3069902e2e8880c
arthpatel-pro <67623435+arthpatel-pro@users.noreply.github.com>
2020-07-02 10:34:17 +0530

    Update Create_VM.bat

--- Create_VM.bat
+++ Create_VM.bat
@@ -1,1 +1,1 @@
-az vm create --resource-group TutorialResources \ --name TutorialVM1 \ --image UbuntuLTS \ --generate-ssh-keys \ --output json \ --verbose
+az vm create --resource-group TutorialResources --name TutorialVM1 --image UbuntuLTS --generate-ssh-keys --output json --verbose
