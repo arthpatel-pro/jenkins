280ba02d7776fb36a6bc33656d9ae6a550645c0c
arthpatel-pro <67623435+arthpatel-pro@users.noreply.github.com>
2020-07-02 10:20:41 +0530

    Update azvmlist.bat

--- azvmlist.bat
+++ azvmlist.bat
@@ -1,1 +1,8 @@
-az vm list
+az group create --name TutorialResources --location eastus
+az vm create --resource-group TutorialResources \
+  --name TutorialVM1 \
+  --image UbuntuLTS \
+  --generate-ssh-keys \
+  --output json \
+  --verbose
+  az vm show --name TutorialVM1 --resource-group TutorialResources
