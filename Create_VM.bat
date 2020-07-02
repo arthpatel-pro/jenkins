2b11b80ebf7d14fadf2eccf8720f9aece6814a63
arthpatel-pro <67623435+arthpatel-pro@users.noreply.github.com>
2020-07-02 11:07:30 +0530

    Update Create_VM.bat

--- Create_VM.bat
+++ Create_VM.bat
@@ -1,1 +1,1 @@
-az vm create --resource-group TutorialResources --name TutorialVM1 --image UbuntuLTS --authentication-type {password} --admin-username arth --admin-password Adminsoftweb@123 --output json --verbose
+az vm create --resource-group TutorialResources --name TutorialVM1 --image UbuntuLTS --authentication-type password --admin-username arth --admin-password Adminsoftweb@123 --output json --verbose
